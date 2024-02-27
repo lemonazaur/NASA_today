dd83b90cf446fef585348c099d07e4d301c0f57b
lemonazaur <serg.hak.work@gmail.com>
2024-02-27 19:25:33 +0400

    Updated design, included new art (src), changed functionality

--- apod.js
+++ apod.js
@@ -1,9 +1,28 @@
 function apod( url ){
     // Change BG to pic of the day
-    document.body.style.backgroundImage = `url('${url}')`;
+    document.body.style.backgroundImage = 'url("src/Moon_ISS.png")';
+    document.body.style.backgroundColor = 'black';
 
     // Clear the content of the .content element
+    // document.querySelector('.msg').innerHTML = '<h1 style="margin: 12px 0">\n' +
+    //     '                GREETINGS CAPITAN !\n' +
+    //     '            </h1>\n' +
+    //     '            <h3 style="margin: 6px 0">THIS IS THE PICTURE OF THE DAY</h3>\n' +
+    //     '            <h3 style="margin: 6px 0">FROM ONE OF OUR COLONIES</h3>' +
+    //     '            <h3 style="margin: 6px 0">TO SEE THE FORECAST, CLICK ON MARS !!!</h3>';
+
     document.querySelector('.content').innerHTML = '';
+       // `<div class="image-container">
+       //     <img class="content-image" alt="PicOfTheDay" src="${url}">
+       // </div>`;
+
+
+
+    //change pic
+    document.querySelector('.nav-pic').src = 'src/MARS.png';
+
+    //change link
+    document.getElementById('change').setAttribute('onclick', 'mars()');
 
     //change UI design to APOD
     document.getElementById('UIset').href = 'APODui.css';
--- mars.js
+++ mars.js
@@ -1,9 +1,25 @@
 function mars(){
     // Clear the content of the .content element
+    //document.querySelector('.msg').innerHTML = '';
+
     document.querySelector('.content').innerHTML = '';
+        // '<div class="image-container">' +
+        // '<iframe src=\'https://mars.nasa.gov/layout/embed/image/insightweather/\' width=\'800\' height=\'530\'  scrolling=\'yes\' frameborder=\'0\'></iframe>' +
+        // '</div>';
+
+
+    //<iframe src='https://mars.nasa.gov/layout/embed/image/insightweather/' width='800' height='530'  scrolling='no' frameBorder='0'></iframe>
+
 
     //set bg MARS
     document.body.style.backgroundImage = 'url(\'src/MarsBG.jpg\')';
+    document.body.style.backgroundColor = 'var(--coral)';
+
+    //change pic
+    document.querySelector('.nav-pic').src = 'src/ISS.png';
+
+    // Properly set the onclick using a function that captures the current value of sessionStorage
+    document.getElementById('change').onclick = function() { apod(sessionStorage.getItem('deckLink')); };
 
     //change UI design to MARS
     document.getElementById('UIset').href = 'MARSui.css';
